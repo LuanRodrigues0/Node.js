02b7335d247269911821c11d8eb6681efd9dd991
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-07-12 22:15:46 -0300

    Update aula03b.js

--- aula03/aula03b.js
+++ aula03/aula03b.js
@@ -1,15 +1,20 @@
-const http = require("http");
-const url = require("url");
-const porta = 3000;
-const host ='127.0.0.1';
+var http = require('http');
+var url = require('url');
+http.createServer(function(req, res){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.writeHead(200, {'Conten-Type': 'appliction/json' });
 
-const Servidor = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write(req.url);
-    const p=url.parse(req.url,true).query;
-    res.write('<br/>'+p.nome);
-    res.write('<br/>'+p.curso);
-    res.end();
-});
+let parametros=url.parse(req.url, true);
+ let mat=parametros.query.matricula;
+  let pas=parametros.query.senha;
 
-Servidor.listen(porta,host,()=>{console.log("Servidor Rodando")});
+  let dados=null
+
+   if(mat=="123" && pas=="321"){
+     dados={
+       nome:"Luan",
+       acesso:18
+     }
+   }
+  res.end(JSON.stringify(dados));
+}).listen(8080);
