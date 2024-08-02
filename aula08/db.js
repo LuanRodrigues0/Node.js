8d9fcacf057b534eaf09bb64be0c6e2b5d15b25d
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-08-01 21:24:51 -0300

    voltei ao node

--- aula08/db.js
+++ aula08/db.js
@@ -4,11 +4,37 @@ const conectar = async ()=>{
         return global.conexao
     }
     const MySQL=require('mysql2/promise')
-    const con=MySQL.createConnection("mysql://root:LUAN123456789@localhost:3306/NODE.JS")
+    const con=MySQL.createConnection("mysql://root:LUAN123456789@localhost:3306/cfbcursos")
     console.log("Conectou ao banco")
     global.conexao=con
     return con
 }
 
-conectar()
-module.exports = {}
+// BUSCANDO A TABELA NO MYSQL
+
+const TodosClientes = async()=>{
+    const con=await conectar()
+    const linhas = con.query('SELECT * FROM cliente_node ')
+    return await linhas
+}
+
+//Aprendendo a fazer inserts
+
+const InseriClientes = async(cliente)=>{
+    const con=await conectar()
+    const sql='INSERT INTO cliente_node (nome,idade) VALUES (?,?)'
+    const valores=[cliente.nome,cliente.idade]
+    await con.query(sql,valores)
+}
+
+//Atualizando os clientes
+
+const AtualizaClientes = async(id,cliente)=>{
+    const con=await conectar()
+    const sql=' UPDATE cliente_node SET nome=?,idade=? WHERE ID =?)'
+    const valores=[cliente.nome,cliente.idade,id]
+    await con.query(sql,valores)
+    
+}
+
+module.exports = {TodosClientes,InseriClientes}
--- aula08/index.js
+++ aula08/index.js
@@ -1,6 +1,31 @@
 //Node com banco MySQL - módulo mysql2 #P1-A [Node e MySQL] - Curso de Node - Aula 08
 
-(async ()=>{
+//Node com banco MySQL - módulo mysql2 #P2-A [Node e MySQL] - Curso de Node - Aula 09
 
+// Node com banco MySQL - módulo mysql2 #P3-A [Node e MySQL] - Curso de Node - Aula 10
+
+(async ()=>{
     const db=require('./db')
+
+    //Inserindo novos clientes
+    //const db=require('./db')
+    //console.log("Inserir novo cliente")
+    //const nom="lndrhrsh"
+    //const ida=28
+
+   // await db.InseriClientes({nome:nom,idade:ida})
+
+
+   
+    
+    const nom="lndrhrsh"
+    const ida=28
+    const id=2
+    await db.InseriClientes({nome:nom,idade:ida})
+    console.log('cliente' + 'atualizado')
+
+
+    console.log("Obter todos os clientes")
+    const clientes=await db.TodosClientes()
+    console.log(clientes)
 })()
